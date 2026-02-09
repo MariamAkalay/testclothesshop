c6842dac35ea352d48fdcc0d86d9056ef68675d1
MariamAkalay <mariam.akalay@gmail.com>
2026-02-09 22:08:38 +0100

    force dynamic fetching for Airtable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { getProducts } from '@/lib/airtable';
 import ShopContent from './ShopContent';
 
+export const dynamic = 'force-dynamic';
 export const revalidate = 60;
 
 export default async function HomePage() {
